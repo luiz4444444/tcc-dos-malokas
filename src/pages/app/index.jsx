0cb35593838840797209067444bfd466df9f728f
luiz <alvesluiz547@gmail.com>
2024-09-29 02:23:50 -0300

    atualização 1.0

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -6,16 +6,20 @@ export default function App() {
   return (
     <div className="App">
       <div className='pagina-inicial pagina'>
-        <h1>ale cortes</h1>
-        <h2>agende seu horario</h2>
-        <h3>online</h3>
-        <p>escolha o serviço, dia e horario <br /> que deseja ser atendido</p>
-        <button>agendar</button>
-        <button>area cliente</button>
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <Link to='/clint'></Link>
+        <header className='cabecalho'>
+          <img src="/assets/images/logo.png" className='logo' />
+          
+          <div className='grupos-textos'>
+            <p>home</p>
+            <p>nossa marca</p>
+            <p>barba e cabelo</p>
+            <p>nossa unidade</p>
+            <p>mais</p>
+          </div>
+          
+        </header>
+        
+        <Link to='/client'>oidsoadi</Link>
       </div>
     </div>
   );
