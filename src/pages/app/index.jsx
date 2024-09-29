03f394f2712622178a69ea16ce36a5fe6f2f9691
vegezika46 <ra08562337579@acaonsfatima.org.br>
2024-09-29 02:59:08 -0300

    cabeçaio

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -7,18 +7,20 @@ export default function App() {
     <div className="App">
       <div className='pagina-inicial pagina'>
         <header className='cabecalho'>
-          <img src="/assets/images/logo.png" className='logo' />
-          
+        <img src="/assets/images/logo.png" className='logo' />
+
           <div className='grupos-textos'>
             <p>home</p>
             <p>nossa marca</p>
             <p>barba e cabelo</p>
             <p>nossa unidade</p>
             <p>mais</p>
+
           </div>
           
+          
         </header>
-        
+      
         <Link to='/client'>oidsoadi</Link>
       </div>
     </div>
