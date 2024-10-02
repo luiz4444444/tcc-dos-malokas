8f368fcf22d966765f60e31c87ece1c9d2cd6669
vegezika46 <ra08562337579@acaonsfatima.org.br>
2024-10-02 17:39:21 -0300

    cabeçaiu

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -4,29 +4,27 @@ import { Link } from 'react-router-dom';
 export default function App() {
 
   return (
-    <div className="App">
-      <div className='pagina-inicial pagina'>
-        <header className='cabecalho'>
-          <div className='teste'>
-            <img src="/assets/images/logo.png" className='logo' />
-          </div>
-
-          <div className='grupos-textos'>
-            <p>home</p>
-            <p>nossa marca</p>
-            <p>barba e cabelo</p>
-            <p>nossa unidade</p>
-            <p>mais</p>
-
-          </div>
-
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor reprehenderit, veniam temporibus nam, vitae nobis dolores excepturi cupiditate minima sit porro aperiam totam eius harum quisquam repellendus, rem fuga eaque.</h1>
-          
-        </header>
-      
-        <Link to='/client'>oidsoadi</Link>
-      </div>
+    <div className="App pagina">
+      <header className='cabecalho'>
+        <div className='area-logo'>
+          <img src="/assets/images/logo.png" className='logo'/>
+        </div>
+        <div className='area-links'>
+          <p>home</p>
+          <p>sobre nos</p>
+          <p>serviços</p>
+          <p>contatos</p>
+        </div>
+        <p>
+          oi
+        </p>
+        <Link to="/client" className='botao'>Clientes</Link>
+      </header>
     </div>
-  );
+
+
+
+
+  )
 }
 
--- src/pages/client/index.jsx
+++ src/pages/client/index.jsx
@@ -4,14 +4,10 @@ import { Link } from 'react-router-dom';
 export default function Client() {
 
     return(
-
         <div className='pagina-cliente pagina'>
-            <h1>ale cortes</h1>
-            <p>faça o login</p>
-            <input type="text" placeholder='Telefone Cadastrado (com DDD)'/>
-            <input type="text" placeholder='senha'/>
-            <Link to='/' className='links'>esqueceu a senha?</Link>
-            <button>entrar</button>
+            <header className='cabecalho'>
+                <img className='logo' src="/assets/images/logo.png"/>
+            </header>
         </div>
     );
 }
