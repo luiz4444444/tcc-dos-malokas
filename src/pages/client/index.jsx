783922998b0c549dd5b98dbcde237d1a3431dbbe
luiz <alvesluiz547@gmail.com>
2024-10-14 08:26:36 -0300

    subindo tcc 1.0.2

--- src/pages/client/index.jsx
+++ src/pages/client/index.jsx
@@ -6,7 +6,12 @@ export default function Client() {
     return(
         <div className='pagina-cliente pagina'>
             <header className='cabecalho'>
-                <img className='logo' src="/assets/images/logo.png"/>
+                <div className='area-logo'>
+                    <img className='logo' src="/assets/images/logo.png"/>
+                </div>
+                <div className='area-links'>
+
+                </div>
             </header>
         </div>
     );
