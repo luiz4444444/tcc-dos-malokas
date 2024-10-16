d1c6d57bccbf4379b67e10ac28fde64624803e81
luiz <alvesluiz547@gmail.com>
2024-10-16 09:43:41 -0300

    suvbinawf

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -5,21 +5,32 @@ export default function App() {
 
   return (
     <div className="App pagina">
+
       <header className='cabecalho'>
-        <div className='area-logo'>
-          <img src="/assets/images/logo.png" className='logo'/>
-        </div>
+        <img src="/assets/images/logo.png" className='logo'/>
         <div className='area-links'>
-          <p>home</p>
-          <p>sobre nos</p>
-          <p>serviços</p>
-          <p>contatos</p>
+          <p>Home</p>
+          <p>Sobre Nós</p>
+          <p>Serviços</p>
         </div>
-        <div className='botao'>
-          <Link to="/client" className='link-botao'>Clientes</Link>
+          <img src="/assets/images/text.png" className='text-foto' />
+        <div className='area-links'>
+          <p>shop</p>
+          <p>contato</p>
         </div>
-        
+        <img src="/assets/images/contat.png" className='contat-foto' />
       </header>
+      <div className='texto'>
+        <h1 className='titulo-text'>Curta nossos serviços de barbeiro</h1>
+        <p className='introducao'>Estabeleça seu novo visual incrível com os serviços da Ale Cortes! </p>
+        <p className='introducao'>De cortes de cabelo até a barbas perfeitas, </p>  
+        <p className='introducao'>você pode experimentar o melhor nivel de barbaria na área</p>
+      </div>
+
+
+        <div className='botao'>
+          <Link to="/client" className='link-botao'>agende aqui</Link>
+        </div>
     </div>
   );
 }
